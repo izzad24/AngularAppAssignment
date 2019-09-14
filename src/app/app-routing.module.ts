b0b34952698d095161d416f043f5a35c64034557
izzad24 <afifizzad_mazlan@dell.com>
2019-09-14 17:58:55 +0800

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { StatPageComponent } from './stat-page/stat-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'stats', component: StatPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
